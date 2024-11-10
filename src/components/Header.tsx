693c99fee265cd1b941b13756a6a311bdbb012ee
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2024-11-10 09:11:18 -0300

    Adjusted details in the navbar focus

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Button from './Button'
+import styles from '../styles/header.module.css'
 
 const Header = () => {
     return (
@@ -6,21 +7,21 @@ const Header = () => {
             <nav className="w-full bg-white">
                 <div className="mx-40 flex flex-row justify-between">
                     <img className="object-contain py-5" src="/assets/logo.svg" alt="logo" />
-                    <ul className="min-w-2/6 flex justify-evenly items-center text-grayishBlue">
+                    <ul className={`${styles.navContainer} min-w-2/6 flex justify-evenly items-center text-grayishBlue`}>
                         <li className=" h-full hover:cursor-pointer hover:text-darkBlue">
-                            <a className="px-5 h-full flex items-center focus:border-b-4 focus:border-b-limeGreen" href="#"><span>Home</span></a>
+                            <a className="px-5 h-full flex items-center" href="#"><span>Home</span></a>
                         </li>
                         <li className="flex h-full hover:cursor-pointer hover:text-darkBlue">
-                            <a className="h-full flex px-5 items-center focus:border-b-4 focus:border-b-limeGreen" href="#"><span>About</span></a>
+                            <a className="h-full flex px-5 items-center" href="#"><span>About</span></a>
                         </li>
                         <li className="flex h-full hover:cursor-pointer hover:text-darkBlue">
-                            <a className="h-full flex px-5 items-center focus:border-b-4 focus:border-b-limeGreen" href="#"><span>Contact</span></a>
+                            <a className="h-full flex px-5 items-center" href="#"><span>Contact</span></a>
                         </li>
                         <li className="flex h-full hover:cursor-pointer hover:text-darkBlue">
-                            <a className="h-full flex px-5 items-center focus:border-b-4 focus:border-b-limeGreen" href="#"><span>Blog</span></a>
+                            <a className="h-full flex px-5 items-center" href="#"><span>Blog</span></a>
                         </li>
                         <li className="flex h-full hover:cursor-pointer hover:text-darkBlue">
-                            <a className="h-full flex px-5 items-center focus:border-b-4 focus:border-b-limeGreen" href="#"><span>Careers</span></a>
+                            <a className="h-full flex px-5 items-center" href="#"><span>Careers</span></a>
                         </li>
                     </ul>
                     <Button />
