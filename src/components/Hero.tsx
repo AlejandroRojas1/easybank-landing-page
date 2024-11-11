3d8931ed142ad4061a948e45041dee847815ee7c
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2024-11-11 14:33:03 -0300

    finish the features section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,16 @@
 import Button from './Button'
+import styles from '../styles/Hero.module.css'
 
 const Hero = () => {
   return (
-    <section className='flex'>
-      <div className='flex flex-col justify-between mt-32 ms-40 w-1/3'>
+    <section className={`${styles.heroContainer} flex justify-between`}>
+      <div className='flex flex-col justify-between my-32 ms-40 w-1/3'>
         <h1 className='text-darkBlue text-6xl leading-tight mb-10 font-light'>Next generation digital banking</h1>
         <p className='text-grayishBlue mb-10'>Take your financial life online. Your Easybank account will be a one-stop-shop 
         for spending, saving, budgeting, investing, and much more.</p>
         <Button moreStyles={`w-1/2`}/>
       </div>
+      <div className={`${styles.illustration}`}></div>
 
     </section>
   )
