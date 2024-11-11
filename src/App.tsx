6a12eb27aaa61b83a40064b1b35f8cb8ea4ce4fc
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2024-11-11 17:34:19 -0300

    finish the Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Articles from "./components/Articles"
 import Features from "./components/Features"
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Features />
       <Articles />
+      <Footer />
     </>
   )
 }
